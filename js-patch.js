ba4516dbcb009e96f6f0a290bc87de59114d8d63
Daniel Lopez <danielxdad@gmail.com>
2019-06-10 23:07:10 -0400

    Se agrega script JS para inyectarlo en el navegador patcheando JS de la pagina.

---
+++ js-patch.js
@@ -0,0 +1,26 @@
+/**
+* Script para parchear JS de la pagina
+* 
+* Snipets:
+*   grecaptcha.reset()
+*   $('[name="g-recaptcha-response"]').val()
+*/
+
+_patch_js_web_page = () => {
+  if ( !confirm('Ya tienes el Google Captcha visible?') ){
+    alert('Pues activalo')
+    return
+  }
+
+  window.originalHandle = () => {}
+  window.handleAjaxComplete = () => {}
+  window.salirSinAjax = () => {}
+  window.cerrarModalExpira = () => {}
+  window.mostrarModalExpira = () => {}
+  clearTimeout(ajaxTimer)
+  clearTimeout(sessionTimer)
+  clearTimeout(sessionCloseTimer)
+  sessionTimeoutSecs = 1000000
+}
+
+_patch_js_web_page()
